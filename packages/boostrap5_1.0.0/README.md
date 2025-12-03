f98f33c4b6ae1729ccd4db632396aac9e9a0e96a
Thorsten Klöhn <thorstenkloehn@gmail.com>
2025-12-03 06:55:58 +0100

    Add Bootstrap 5 resources and initial configuration
    - Added favicon.ico and logo SVGs for Bootstrap 5.
    - Created main.js with a console log message.
    - Introduced SCSS files for theme variables and main theme.
    - Added composer.json for package management with dependencies.
    - Configured ext_emconf.php for TYPO3 extension metadata.
    - Set up ext_localconf.php for default RTE configuration.
    - Created ext_tables.php for TYPO3 extension table definitions.

---
+++ packages/boostrap5_1.0.0/README.md
@@ -0,0 +1,4 @@
+Site Package for the project "Boostrap5"
+==============================================================
+
+Add some explanation here.
---
+++ packages/boostrap5_1.0.0/ext_localconf.php
@@ -0,0 +1,6 @@
+<?php
+
+defined('TYPO3') or die('Access denied.');
+
+// Add default RTE configuration
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['boostrap5'] = 'EXT:boostrap5/Configuration/RTE/Default.yaml';
